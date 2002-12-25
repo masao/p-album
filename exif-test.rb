2c2bfc039d9a1c46b916cf0216b1a292261805ca
masao <tmasao@acm.org>
2002-12-25 20:55:20 +0000

    tainted? のテストも追加。

--- exif-test.rb
+++ exif-test.rb
@@ -6,13 +6,20 @@ require 'exif'
 def print_tag (exif, str)
   begin
     val = exif[str]
-    print "#{str}\t#{val}\n"
+    print "#{str}\t#{val}"
+    if val.tainted?
+        puts "(tainted)"
+    else 
+	puts "(untainted)" 
+    end
   rescue Exif::Error => e
     STDERR.print "#{str}\t(not found)\n"
   end
 end
 
 exif = Exif.new(ARGV[0])
+p exif.ifd
+exif.ifd = "0" # Exif::IFD::Zero
 print_tag(exif, 'Make')
 print_tag(exif, 'Model')
 print_tag(exif, 'Software')
